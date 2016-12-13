297af1adbb603ebdf81329e7c368010a6a63b6b9
al <al@twilio.com>
2016-12-13 17:27:57 +0100

    update

--- index.js
+++ index.js
@@ -18,4 +18,9 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+app.get('/alive', function(request, response) {
+ 
+  response.send('I AM ALIVE');
+});
+
 
