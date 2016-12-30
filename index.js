77ae2344efb29b32df0f5b31aedcc66872ffc6c5
al <al@twilio.com>
2016-12-30 11:33:15 -0800

    update

--- index.js
+++ index.js
@@ -91,7 +91,8 @@ function createTask(attributesString, fn) {
 function checkForExistingTask(CallSid, fn) {
 	console.log("checking for any existing task for this call SID");
 	var taskSid=false;
-	var queryJson['EvaluateTaskAttributes'] = "(CallSid=\"" + request.body['CallSid'] + "\")";
+	var queryJson = {};
+	queryJson['EvaluateTaskAttributes'] = "(CallSid=\"" + request.body['CallSid'] + "\")";
 	client.workspace.tasks.get(queryJson, function(err, data) {
         if (!err) {
             // looping through them, but call SIDs are unique and should only ever be one task maximum 	
