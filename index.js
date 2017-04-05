81cd2f4c7c1d93d59fdb6ef22bb8ef5704c174cc
al cook <al@twilio.com>
2017-04-05 15:25:23 -0700

    visualizer

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ function updateTask(attributesJson, task, fn) {
 
 function forceTaskRefresh(taskSid, fn) {
 	client.workspace.workflows("WW404ac496bea3aa47579de9204d661e77").get(function(err, workflow) {
-    console.log(JSON.parse(workflow));
+    console.log(workflow);
 });
 	var tempOptions = {
 	      method: 'POST',
